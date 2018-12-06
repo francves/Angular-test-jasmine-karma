3381ebd8e61fa3feeee8908a9acce78d3e272b19
Francisco Velasquez <francves1711@gmail.com>
2018-12-06 14:55:18 -0400

    User service y test unitario de funcion get

---
+++ src/app/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class UsersService {
+   path: string = 'http://jsonplaceholder.typicode.com/users';
+   constructor(
+    private http: Http
+  ) {}
+   getUser(id: number): Observable<any>{
+    return this.http.get(`${this.path}/${id}`)
+    .pipe(map( response => response.json()));
+    }
+ }
